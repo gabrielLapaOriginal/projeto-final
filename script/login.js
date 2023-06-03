fc2c214038312b1bb6bbc41294921b5af8f73d53
Gabriel Henrique Lapa <111655328+gabrielLapaOriginal@users.noreply.github.com>
2023-06-03 18:11:48 -0300

    Add files via upload

---
+++ script/login.js
@@ -0,0 +1,17 @@
+var formLogin = document.querySelector('#login');
+var formCadastro = document.querySelector('#cadastro');
+var btnColor = document.querySelector('.btnColor');
+
+document.querySelector('#btnLogin')
+.addEventListener('click', ()=>{
+    formLogin.style.left = "25px"
+    formCadastro.style.left = "450px"
+    btnColor.style.left = "0px"
+})
+
+document.querySelector('#btnCadastro')
+.addEventListener('click', ()=>{
+    formLogin.style.left = "-450px"
+    formCadastro.style.left = "25px"
+    btnColor.style.left = "110px"
+})
